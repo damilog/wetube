8da43e5b0fc933c4ac960ac82feaca9e3a2c5362
damilog <dami.k.dev@gmail.com>
2020-07-05 23:44:13 +0900

    Feat : Express Core
    - 2.6 Express Core : Middlewares

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+//const express = require("express");
+import express from "express";
+
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () =>{
-    console.log(`Listening on : http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res){
-    res.send("Hello from Home!");
-} //하나는 request obj, 다른 하나는 response obj
+const handleHome=(req, res) => res.send("Hello from home~~~"); // 유저에게 반환하는 값, 반환할 게 없다면 계속 loading일 것
 
-function handleProfile(req, res){
-    res.send("you are on my profile!");
-}
-app.get("/",handleHome); 
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {   
+    console.log('Between');
+    next();
+};
+
+app.use(betweenHome); // 미들웨어 순서가 중요함. 접속이 있을때 위부터 아래로 시작함 // middelware -> route 순서로 작동
+
+app.get("/", handleHome);  // "/"와 handleHome 사이에 betweenHome가 있음
+
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
