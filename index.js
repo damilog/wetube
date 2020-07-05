616cf9201f15e9df9188977fcf99bcee298c5e79
damilog <dami.k.dev@gmail.com>
2020-07-06 00:07:32 +0900

    2.7 Express Core: Middlewares part Two
    - Add morgan, helmet, cookieparser, bodyparser

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-//const express = require("express");
 import express from "express";
-
+import morgan  from "morgan";
+import helmet  from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from"body-parser";
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => 
+ console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome=(req, res) => res.send("Hello from home~~~"); // 유저에게 반환하는 값, 반환할 게 없다면 계속 loading일 것
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {   
-    console.log('Between');
-    next();
-};
-
-app.use(betweenHome); // 미들웨어 순서가 중요함. 접속이 있을때 위부터 아래로 시작함 // middelware -> route 순서로 작동
-
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+ 
 app.get("/", handleHome);  // "/"와 handleHome 사이에 betweenHome가 있음
 
 app.get("/profile", handleProfile);
