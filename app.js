14d5b1787904cfbe98219d61976f5c6af4ddce17
damilog <dami.k.dev@gmail.com>
2020-07-12 14:38:28 +0900

    #2.8 Express Core: Routing
    - change index.js -> app.js
    - Add router.js

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import morgan  from "morgan";
 import helmet  from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from"body-parser";
+import {userRouter} from "./router";
+
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => 
- console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome=(req, res) => res.send("Hello from home~~~"); // 유저에게 반환하는 값, 반환할 게 없다면 계속 loading일 것
 
@@ -24,4 +21,7 @@ app.get("/", handleHome);  // "/"와 handleHome 사이에 betweenHome가 있음
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+
+export default app; 
