07a2c5a7376bd0ca512b06446370e6a06d27c5ec
damilog <dami.k.dev@gmail.com>
2020-07-12 20:57:27 +0900

    #2.9 MVC Pattern part One
    - Add global, user and video Routers

--- app.js
+++ app.js
@@ -3,25 +3,19 @@ import morgan  from "morgan";
 import helmet  from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from"body-parser";
-import {userRouter} from "./router";
-
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from"./routers/globalRouter";
 const app = express();
-
-const handleHome=(req, res) => res.send("Hello from home~~~"); // 유저에게 반환하는 값, 반환할 게 없다면 계속 loading일 것
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
- 
-app.get("/", handleHome);  // "/"와 handleHome 사이에 betweenHome가 있음
 
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
-
+app.use("/video", videoRouter);
 
 export default app; 
